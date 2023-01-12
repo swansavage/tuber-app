efd93a9e5a8ab348ea64bce44526b54f3a2068a5
swansavage@github.com <swansavage@gmail.com>
2023-01-12 13:29:57 -0600

    setting up Feed component, added sidebar with unstyled categories

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { Category } from "@mui/icons-material";
+import { Stack } from "@mui/material";
+
+import { categories } from "../utils/constants";
+
+const Sidebar = () => {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((category) => (
+        <button>
+          <span>{category.icon}</span>
+          <span>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  );
+};
+
+export default Sidebar;
