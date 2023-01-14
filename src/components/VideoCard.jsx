e83b06c0aa3d244b02c1d86f6b17f805b1f5f28b
swansavage@github.com <swansavage@gmail.com>
2023-01-14 14:00:42 -0600

    fleshed out VideoCard component to display video title and channel info

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -17,7 +17,13 @@ const VideoCard = ({
 	},
 }) => {
 	return (
-		<Card>
+		<Card
+			sx={{
+				width: { md: '320px', sm: '100%', xs: '100%' },
+				boxShadow: 'none',
+				borderRadius: 0,
+			}}
+		>
 			<Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
 				<CardMedia
 					image={snippet?.thumbnails?.high?.url}
@@ -25,6 +31,25 @@ const VideoCard = ({
 					sx={{ width: 358, height: 180 }}
 				/>
 			</Link>
+			<CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+				<Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+					<Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+						{snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+					</Typography>
+				</Link>
+				<Link
+					to={
+						snippet?.channelId
+							? `/channel/${snippet?.channelId}`
+							: demoChannelUrl
+					}
+				>
+					<Typography variant="subtitle2" fontWeight="bold" color="gray">
+						{snippet?.channelTitle || demoChannelTitle}
+						<CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
+					</Typography>
+				</Link>
+			</CardContent>
 		</Card>
 	);
 };
