220c1ac908f860da9a61be1f54429fdfb3130b12
swansavage@github.com <swansavage@gmail.com>
2023-01-15 20:47:14 -0600

    fleshed out ChannelDetail component, styled, pulls data from API

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { demoProfilePicture } from '../utils/constants';
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
 	console.log(channelDetail);
 	return (
 		<Box
@@ -17,6 +17,7 @@ const ChannelCard = ({ channelDetail }) => {
 				width: { xs: '356px', md: '320px' },
 				height: '326px',
 				margin: 'auto',
+				marginTop: marginTop,
 			}}
 		>
 			<Link to={`/channel/${channelDetail?.id?.channelId}`}>
